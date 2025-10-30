708efc1b295fe51858707afa86dfec97c64d1387
dydhyun <dydgus625@naver.com>
2025-10-30 20:06:31 +0900

    feat: login요청시 헤더에 accessToken담아보내기

--- src/main/java/com/yh/blogserver/controller/user/UserController.java
+++ src/main/java/com/yh/blogserver/controller/user/UserController.java
@@ -1,8 +1,10 @@
 package com.yh.blogserver.controller.user;
 
+import com.yh.blogserver.config.JwtTokenProvider;
 import com.yh.blogserver.dto.ResponseDto;
 import com.yh.blogserver.dto.UserDto;
 import com.yh.blogserver.service.user.UserService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +19,11 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/userId-check")
@@ -76,11 +80,18 @@ public class UserController {
 
         UserDto loginedUserDto = userService.login(loginRequest);
 
+        String token = jwtTokenProvider.createToken(loginedUserDto.getUserId(), loginedUserDto.getIsAdmin());
+//        String refreshToken = jwtTokenProvider.createRefreshToken(loginedUserDto.getUserId());
+        
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization","Bearer" + token);
+        // http 표준 규약 -> Authorization: <type> <credentials>
+
         responseDto.setItem(loginedUserDto);
         responseDto.setStatusCode(HttpStatus.OK.value());
         responseDto.setStatusMessage("LOGIN SUCCESS");
 
-        return ResponseEntity.status(responseDto.getStatusCode()).body(responseDto);
+        return ResponseEntity.status(responseDto.getStatusCode()).headers(httpHeaders).body(responseDto);
     }
 
 
