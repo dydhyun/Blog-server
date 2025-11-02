25fa92fdd6623827233b4edcad4de3222814f72a
dydhyun <dydgus625@naver.com>
2025-11-02 19:29:55 +0900

    feat: board 엔티티,dto 추가

---
+++ src/main/java/com/yh/blogserver/dto/BoardDto.java
@@ -0,0 +1,35 @@
+package com.yh.blogserver.dto;
+
+import com.yh.blogserver.entitiy.Board;
+import com.yh.blogserver.entitiy.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDto {
+
+    public Long boardIndex;
+    public User user;
+    public String boardTitle;
+    public String boardContents;
+    public LocalDateTime boardCreatedTime;
+    public Long boardViewCnt;
+
+    public Board toEntity(){
+        return Board.builder()
+                .boardIndex(this.boardIndex)
+                .user(this.user)
+                .boardTitle(this.boardTitle)
+                .boardContents(this.boardContents)
+                .boardCreatedTime(this.boardCreatedTime)
+                .boardViewCnt(this.boardViewCnt)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/yh/blogserver/entity/Board.java
@@ -0,0 +1,51 @@
+package com.yh.blogserver.entity;
+
+import com.yh.blogserver.dto.BoardDto;
+import jakarta.persistence.*;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+public class Board {
+    protected Board() {} // jpa 자체 에서만 사용하는 기본생성자.
+
+    public Board(User user, String title, String contents) {
+        this.user = user;
+        this.boardTitle = title;
+        this.boardContents = contents;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardIndex;
+
+    @ManyToOne
+    @JoinColumn(name = "writer", referencedColumnName = "userIndex")
+    private User user;
+    private String boardTitle;
+    private String boardContents;
+
+    @Column(updatable = false)
+    private LocalDateTime boardCreatedTime;
+    private Long boardViewCnt;
+
+    @PrePersist
+    private void prePersist(){
+        this.boardCreatedTime = LocalDateTime.now();
+        this.boardViewCnt = 0L;
+    }
+
+    public BoardDto toDto(){
+        return BoardDto.builder()
+                .boardIndex(this.boardIndex)
+                .user(this.user)
+                .boardTitle(this.boardTitle)
+                .boardContents(this.boardContents)
+                .boardCreatedTime(this.boardCreatedTime)
+                .boardViewCnt(this.boardViewCnt)
+                .build();
+    }
+
+}
