62fb2a26a1c14c4acd3107b99394f083f0833e9a
dydhyun <dydgus625@naver.com>
2025-11-11 00:07:04 +0900

    refactor: 수정된 responseDto 적용

--- src/main/java/com/yh/blogserver/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yh/blogserver/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.yh.blogserver.exception;
 
-import com.yh.blogserver.dto.ResponseDto;
+import com.yh.blogserver.dto.response.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,27 +8,34 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // 200 :
+    // 201 :
+    // 202 : 비동기
+    // 204 :
     // 400 : BAD_REQUEST : 잘못된 요청
+    // 401 :
+    // 403 :
+    // 404 :
+    // 405 :
+    // 408 :
+    // 409 :
+    // 415 :
     // 500 : INTERNAL_SERVER_ERROR : 서버에러 발생 <- 최상위 에러 Exception에 매핑하기
+    // 502 :
+    // 504 :
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ResponseDto<?>> handleIllegalArgument(IllegalArgumentException e) {
-        ResponseDto<Object> responseDto = new ResponseDto<>();
-        responseDto.setStatusCode(HttpStatus.BAD_REQUEST.value());
-        responseDto.setStatusMessage("BAD_REQUEST");
-        responseDto.setErrorMessage(e.getMessage());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseDto.error(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseDto<?>> handleGeneralException(Exception e) {
-        ResponseDto<Object> responseDto = new ResponseDto<>();
-        responseDto.setStatusCode(HttpStatus.BAD_REQUEST.value());
-        responseDto.setStatusMessage("BAD_REQUEST");
-        responseDto.setErrorMessage(e.getMessage());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDto);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseDto.error(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
 
 }
