4705b431575c3c02d0448f8fdb9590dafaa061fd
dydhyun <dydgus625@naver.com>
2025-11-12 19:16:45 +0900

    refactort: userEntity - softDelete용도 userDeleteFlag 추가

--- src/main/java/com/yh/blogserver/entity/User.java
+++ src/main/java/com/yh/blogserver/entity/User.java
@@ -36,10 +36,17 @@ public class User {
 
     @Column(updatable = false)
     private LocalDateTime createdDate;
+    private Boolean userDeleteFlag;
 
+    public void markAsDeleted() {
+        this.userDeleteFlag = true;
+    }
+    // Lombok 의 builder , AllArgsConstructor 로 객체 생성시 기본필드 초기값 무시됨.
+    // @PrePersist 는 JPA 가 persist() 직전에 실행해서 안전하게 모든 생성 방식에서 빠진 필드값을 채워넣을 수 있음.
     @PrePersist
     private void prePersist(){
         this.isAdmin = false;
         this.createdDate = LocalDateTime.now();
+        this.userDeleteFlag = false;
     }
 }
