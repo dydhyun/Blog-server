fe101c7610668201f0b261f3c09b8ca415c77347
dydhyun <dydgus625@naver.com>
2025-11-13 19:44:34 +0900

    feat: userController - swagger api 명세 추가

--- src/main/java/com/yh/blogserver/controller/user/UserController.java
+++ src/main/java/com/yh/blogserver/controller/user/UserController.java
@@ -6,6 +6,9 @@ import com.yh.blogserver.dto.request.UserRequestDto;
 import com.yh.blogserver.dto.response.UserResponseDto;
 import com.yh.blogserver.service.user.UserService;
 import com.yh.blogserver.util.message.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -30,6 +33,11 @@ public class UserController {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
+    @Operation(summary = "아이디 중복체크", description = "회원가입 폼에서 유저아이디 문자열을 받아 중복 체크 하기 위한 엔드포인트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "사용 가능한 아이디"),
+            @ApiResponse(responseCode = "400", description = "사용할 수 없는 아이디")
+    })
     @GetMapping("/exists/id")
     public ResponseEntity<ResponseDto<Map<String, String>>> userIdCheck(@RequestParam String userId){
         log.info("[userIdCheck 요청] userId={}", userId);
@@ -40,6 +48,11 @@ public class UserController {
                 .ok(ResponseDto.success(checkMsgMap, HttpStatus.OK, checkMsgMap.get(CHECK_MSG_KEY)));
     }
 
+    @Operation(summary = "닉네임 중복체크", description = "회원가입 폼에서 유저닉네임 문자열을 받아 중복 체크 하기 위한 엔드포인트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "사용 가능한 닉네임"),
+            @ApiResponse(responseCode = "400", description = "사용할 수 없는 닉네임")
+    })
     @GetMapping("/exists/nickname")
     public ResponseEntity<ResponseDto<Map<String, String>>> userNicknameCheck(@RequestParam String userNickname){
         log.info("[userNicknameCheck 요청] userNickname={}", userNickname);
@@ -51,6 +64,11 @@ public class UserController {
     }
 
     // 비밀번호는 검증이기에 postMapping
+    @Operation(summary = "비밀번호 검증", description = "회원가입 폼에서 유저비밀번호 문자열을 받아 유효성 검사 하기 위한 엔드포인트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "사용 가능한 비밀번호"),
+            @ApiResponse(responseCode = "400", description = "사용할 수 없는 비밀번호")
+    })
     @PostMapping("/verify-pw")
     public ResponseEntity<ResponseDto<Map<String, String>>> userPwCheck(@RequestParam String userPw){
         log.info("[userPwCheck 요청] userPw={}", userPw);
@@ -61,6 +79,11 @@ public class UserController {
                 .ok(ResponseDto.success(checkMsgMap, HttpStatus.OK, checkMsgMap.get(CHECK_MSG_KEY)));
     }
 
+    @Operation(summary = "유저 회원가입", description = "회원가입 폼에서 유저입력 정보를 받아 db 등록을 위한 엔드포인트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "회원가입 실패")
+    })
     @PostMapping("")
     public ResponseEntity<ResponseDto<UserResponseDto>> join(@RequestBody UserRequestDto userRequestDto){
         log.info("[USER JOIN 요청] userRequestDto={}", userRequestDto);
@@ -71,6 +94,11 @@ public class UserController {
                 .body(ResponseDto.success(userResponseDto, HttpStatus.CREATED, ResponseMessage.CREATED.message()));
     }
 
+    @Operation(summary = "로그인", description = "아이디와 비밀번호를 userRequestDto 형태로 받아 로그인 하고 토큰 발급하기 위한 엔드포인트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "400", description = "로그인 실패")
+    })
     @PostMapping("/login")
     public ResponseEntity<ResponseDto<UserResponseDto>> login(@RequestBody UserRequestDto loginRequest){
         log.info("[USER LOGIN 요청] userRequestDto={}", loginRequest);
@@ -87,8 +115,5 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).headers(httpHeaders)
                 .body(ResponseDto.success(loginedUserDto, HttpStatus.OK, ResponseMessage.OK.message()));
     }
-    
-    // 탈퇴기능 추가하기
-
 
 }
