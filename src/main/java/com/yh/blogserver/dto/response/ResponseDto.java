2b71a8a88be638b53bb621b269070277cfd7c054
dydhyun <dydgus625@naver.com>
2025-11-16 20:28:44 +0900

    chore: swagger 어노테이션 추가

--- src/main/java/com/yh/blogserver/dto/response/ResponseDto.java
+++ src/main/java/com/yh/blogserver/dto/response/ResponseDto.java
@@ -1,12 +1,19 @@
 package com.yh.blogserver.dto.response;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
+@Schema(description = "API 공통 응답 Dto")
 public record ResponseDto<T> (
-    T data,
-    HttpStatus status,
-    String message
+        @Schema(description = "응답 데이터")
+        T data,
+
+        @Schema(description = "HTTP Status")
+        HttpStatus status,
+
+        @Schema(description = "응답 메시지")
+        String message
 ) {
     public static <T> ResponseDto<T> success(T data, HttpStatus status, String message) {
         return new ResponseDto<>(data, status, message);
