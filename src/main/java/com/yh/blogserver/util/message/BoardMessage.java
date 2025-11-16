0e745e037476fd5da0a5fbfc4fd4af3657cf9305
dydhyun <dydgus625@naver.com>
2025-11-16 20:22:28 +0900

    BoardMessage Enum 필드 추가

--- src/main/java/com/yh/blogserver/util/message/BoardMessage.java
+++ src/main/java/com/yh/blogserver/util/message/BoardMessage.java
@@ -3,6 +3,7 @@ package com.yh.blogserver.util.message;
 public enum BoardMessage {
     ALREADY_DELETED("이미 삭제된 게시글 입니다."),
     FORBIDDEN_DELETE("작성자만 삭제할 수 있습니다."),
+    WRONG_WRITER("게시글 작성자가 아닙니다."),
     BOARD_NOT_FOUND("게시글을 찾을 수 없습니다.");
 
     private final String message;
