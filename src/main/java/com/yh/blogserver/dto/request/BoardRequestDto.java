262d9f8433bdde31924f861989b319e949ec654c
dydhyun <dydgus625@naver.com>
2025-11-20 16:24:51 +0900

    chore: swagger ui schema 예시 추가

--- src/main/java/com/yh/blogserver/dto/request/BoardRequestDto.java
+++ src/main/java/com/yh/blogserver/dto/request/BoardRequestDto.java
@@ -1,17 +1,24 @@
 package com.yh.blogserver.dto.request;
 
 import com.yh.blogserver.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder(toBuilder = true)
 public record BoardRequestDto(
+        @Schema(description = "게시글 인덱스. 사용자 입력 x", example = "1")
         Long boardIndex,
+        @Schema(description = "게시글 작성자. 사용자 입력 x", example = "유저인덱스를 담고있음.")
         User user,
+        @Schema(description = "게시글 제목. 빈 제목 불가", example = "게시글 1")
         String boardTitle,
+        @Schema(description = "게시글 내용. 빈 내용 불가", example = "게시글 1 내용 입니다.")
         String boardContents,
+        @Schema(description = "게시글 생성시간. 사용자 입력 x", example = "2025-11-20 17:42:26.517182")
         LocalDateTime boardCreatedTime,
+        @Schema(description = "게시글 조회수. 사용자 입력 x", example = "1")
         Long boardViewCnt
 ) {
 }
--- src/main/java/com/yh/blogserver/dto/request/UserRequestDto.java
+++ src/main/java/com/yh/blogserver/dto/request/UserRequestDto.java
@@ -1,17 +1,26 @@
 package com.yh.blogserver.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "회원가입 등 사용자가 프론트에서 요청하는 DTO")
 @Builder
 public record UserRequestDto(
 //        Long userIndex,
+        @Schema(description = "로그인용 사용자 ID. 8글자 이상 공백 불가", example = "test1234")
         String userId,
+        @Schema(description = "로그인용 사용자 비밀번호. 8글자 이상 특수문자 포함", example = "test1234!")
         String userPw,
+        @Schema(description = "사용자 이름.", example = "안용현")
         String username,
+        @Schema(description = "사용자 닉네임. 공백불가", example = "myNickname")
         String nickname,
+        @Schema(description = "사용자 주소.", example = "대구광역시 북구")
         String address,
         String addressDetail,
+        @Schema(description = "사용자 핸드폰 번호. 숫자만 입력받아 문자열로 저장", example = "01012342365")
         String pNumber,
+        @Schema(description = "사용자 이메일.", example = "dydgus625@naver.com")
         String email
 ) {
 }
