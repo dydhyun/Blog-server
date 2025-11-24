7c9f44b46a14cd826ddb8b79c16eee3a8dfd7415
dydhyun <dydgus625@naver.com>
2025-11-24 23:15:01 +0900

    chore: swagger ui  schema 설명 추가

--- src/main/java/com/yh/blogserver/dto/request/BoardRequestDto.java
+++ src/main/java/com/yh/blogserver/dto/request/BoardRequestDto.java
@@ -6,11 +6,12 @@ import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "게시글 생성,수정 등 사용자가 프론트에서 요청하는 DTO")
 @Builder(toBuilder = true)
 public record BoardRequestDto(
         @Schema(description = "게시글 인덱스. 사용자 입력 x", example = "1")
         Long boardIndex,
-        @Schema(description = "게시글 작성자. 사용자 입력 x", example = "유저인덱스를 담고있음.")
+        @Schema(description = "게시글 작성자. 사용자 입력 x", example = "토큰으로 유저아이디 추출해서 유저 정보 세팅중.")
         User user,
         @Schema(description = "게시글 제목. 빈 제목 불가", example = "게시글 1")
         String boardTitle,
--- src/main/java/com/yh/blogserver/dto/response/BoardResponseDto.java
+++ src/main/java/com/yh/blogserver/dto/response/BoardResponseDto.java
@@ -1,9 +1,11 @@
 package com.yh.blogserver.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "서버에서 응답으로 돌려보내는 게시글관련 DTO")
 @Builder
 public record BoardResponseDto(
         Long boardIndex,
