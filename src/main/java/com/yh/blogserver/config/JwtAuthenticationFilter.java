dfa97b1381503f5420c534cc2a5f531408e8ae9f
dydhyun <dydgus625@naver.com>
2025-12-15 00:33:44 +0900

    add: JwtAuthenticationFilter token 없을경우 null토큰처리용 얼리리턴, 토큰검증메서드 오류발생시 얼리리턴, parseJwt메서드 리팩토링

--- src/main/java/com/yh/blogserver/config/JwtAuthenticationFilter.java
+++ src/main/java/com/yh/blogserver/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.yh.blogserver.config;
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,39 +30,75 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String token = parseJwt(request);
 
-        if (token != null && jwtTokenProvider.validateToken(token)){
-            String userId = jwtTokenProvider.getUserIdFromToken(token);
-
-            boolean isAdmin = jwtTokenProvider.getUserGrantFromToken(token);
-            // SecurityContext에 인증 객체 생성
-            // authorities 는 Collection<? extends GrantedAuthority> 타입.
-            // GrantedAuthority → 스프링 시큐리티에서 권한(roles, 권한 이름) 을 표현하는 인터페이스
-            // 한 사용자에게 여러 권한이 있을 수 있기 때문에 리스트나 세트 형태로 제공
-            List<GrantedAuthority> authorities = new ArrayList<>();
-            // springSecurity 규약 -> ROLE prefix
-            if (isAdmin) {
-                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            } else {
-                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-            }
-
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(userId, null, authorities);
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (token == null) {
+            //토큰이 없는 요청은 인증 대상이 아니므로
+            //불필요한 JWT 검증 로직을 타지 않도록 early return
+            filterChain.doFilter(request, response);
+            return;
         }
+
+        try {
+            jwtTokenProvider.validateToken(token);
+        } catch (JwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write("""
+            {"message": "인증 토큰이 유효하지 않습니다"}
+            """);
+            return;
+            //SpringMVC 를 타지않고,
+            //다음과 같이 응답을 만들어 클라이언트에 즉시 응답함.
+            //HTTP/1.1 401 Unauthorized
+            //Content-Type: application/json
+            //
+            //{"message": "인증 토큰이 유효하지 않습니다"}
+        }
+
+        String userId = jwtTokenProvider.getUserIdFromToken(token);
+        boolean isAdmin = jwtTokenProvider.getUserGrantFromToken(token);
+
+        // SecurityContext에 인증 객체 생성
+        // authorities 는 Collection<? extends GrantedAuthority> 타입.
+        // GrantedAuthority → 스프링 시큐리티에서 권한(roles, 권한 이름) 을 표현하는 인터페이스
+        // 한 사용자에게 여러 권한이 있을 수 있기 때문에 리스트나 세트 형태로 제공
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        // springSecurity 규약 -> ROLE prefix
+        if (isAdmin) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        } else {
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        // 인증 성공 → SecurityContext 에 Authentication 세팅
+        // Principal 객체로 사용자 ID 대신 CustomUserDetails 객체 사용 가능
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(userId, null, authorities);
+
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         filterChain.doFilter(request, response);
     }
 
     private String parseJwt(HttpServletRequest request) {
-        String encodedToken = request.getHeader("Authorization");
+        String headerAuth = request.getHeader("Authorization");
 
-        if (StringUtils.hasText(encodedToken) && encodedToken.startsWith("Bearer")){
-            return encodedToken.substring(7);
+        if (!StringUtils.hasText(headerAuth)) {
+            return null;
         }
 
-        return null;
+        if (!headerAuth.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return headerAuth.substring(7).trim();
+
+//        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")){
+//            return headerAuth.substring(7);
+//        }
+//
+//        return null;
     }
 
 }
