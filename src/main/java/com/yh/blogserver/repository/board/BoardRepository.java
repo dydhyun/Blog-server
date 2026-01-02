3ac5285303bbd19868b36d0f891f85573acaf209
dydhyun <dydgus625@naver.com>
2026-01-02 15:58:50 +0900

    feat: BoardRepository 스케쥴러용 게시글 물리삭제 쿼리 추가

--- src/main/java/com/yh/blogserver/repository/board/BoardRepository.java
+++ src/main/java/com/yh/blogserver/repository/board/BoardRepository.java
@@ -2,12 +2,20 @@ package com.yh.blogserver.repository.board;
 
 import com.yh.blogserver.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    void deleteByBoardIndex(Long boardIndex);
-
-    boolean existsByBoardIndexAndUser_UserId(Long boardIndex, String userId);
+    @Modifying
+    @Query("""
+        DELETE FROM Board b
+        WHERE b.boardDeleteFlag = true
+        AND b.boardDeletedAt <= :expiredTime
+""")
+    int deleteExpiredBoard(LocalDateTime expiredTime);
 }
