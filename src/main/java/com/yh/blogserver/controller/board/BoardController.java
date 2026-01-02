a0b5d3aa2ffe3ede85b9d2faaac4f555609e88f8
dydhyun <dydgus625@naver.com>
2026-01-02 15:55:53 +0900

    fix: BoardController 불필요한 의존성 삭제

--- src/main/java/com/yh/blogserver/controller/board/BoardController.java
+++ src/main/java/com/yh/blogserver/controller/board/BoardController.java
@@ -4,30 +4,26 @@ import com.yh.blogserver.dto.request.BoardRequestDto;
 import com.yh.blogserver.dto.response.BoardResponseDto;
 import com.yh.blogserver.dto.response.ResponseDto;
 import com.yh.blogserver.service.board.BoardService;
-import com.yh.blogserver.service.user.UserService;
 import com.yh.blogserver.util.message.ResponseMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Tag(name = "Board API", description = "게시글 CRUD 관련 API")
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
 
-    private static final Logger log = LoggerFactory.getLogger(BoardController.class);
-    private final UserService userService;
     private final BoardService boardService;
 
-    public BoardController(UserService userService, BoardService boardService) {
-        this.userService = userService;
+    public BoardController(BoardService boardService) {
         this.boardService = boardService;
     }
 
@@ -110,6 +106,5 @@ public class BoardController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.success(null, ResponseMessage.DELETED.message(), HttpStatus.OK.value()));
     }// 204 NO_CONTENT 는 body가 없음. -> ResponseEntity.noContent().build()
-// userService 생성자주입 확인하기
 
 }
