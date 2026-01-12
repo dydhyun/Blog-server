46fcb90b4ec34868bff6e7afa643320c3455d19b
dydhyun <dydgus625@naver.com>
2026-01-12 19:18:21 +0900

    add: JWT관련 토큰부재,만료,위조 분기 예외처리용 클래스 추가

---
+++ src/main/java/com/yh/blogserver/config/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,53 @@
+package com.yh.blogserver.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yh.blogserver.config.security.message.AuthErrorMessage;
+import com.yh.blogserver.config.security.response.SecurityErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+// JWT 관련 토큰없음, 인증실패 예외 분기
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException
+    ) throws IOException, ServletException {
+        AuthErrorMessage authErrorMessage = resolveError(authException);
+
+        response.setStatus(authErrorMessage.status().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(
+                objectMapper.writeValueAsString(
+                        SecurityErrorResponse.from(authErrorMessage)
+                )
+        );
+    }
+
+
+    private AuthErrorMessage resolveError(AuthenticationException exception) {
+
+        Throwable cause = exception.getCause();
+
+        if (cause instanceof ExpiredJwtException) {
+            return AuthErrorMessage.EXPIRED_TOKEN;
+        }
+
+        if (cause instanceof JwtException) {
+            return AuthErrorMessage.INVALID_TOKEN;
+        }
+
+        return AuthErrorMessage.TOKEN_REQUIRED;
+
+    }
+}
