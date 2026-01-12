9104b1ab1fd3592558ca4dfde7974d76cd6e7ea4
dydhyun <dydgus625@naver.com>
2026-01-12 19:15:53 +0900

    add: 권한부족 예외처리용 클래스 추가

---
+++ src/main/java/com/yh/blogserver/config/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.yh.blogserver.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yh.blogserver.config.security.message.AuthErrorMessage;
+import com.yh.blogserver.config.security.response.SecurityErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+// 권한부족 예외 분기
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+        AuthErrorMessage authErrorMessage = AuthErrorMessage.ACCESS_DENIED;
+
+        response.setStatus(authErrorMessage.status().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(
+                objectMapper.writeValueAsString(
+                        SecurityErrorResponse.from(authErrorMessage)
+                )
+        );
+    }
+}
