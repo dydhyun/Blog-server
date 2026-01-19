1483f0b43eb07872fbe23f46ed93850e796b6ff0
dydhyun <dydgus625@naver.com>
2026-01-19 23:09:45 +0900

    docs: BoardSearchCondition swagger api 명세 추가

--- src/main/java/com/yh/blogserver/dto/request/BoardSearchCondition.java
+++ src/main/java/com/yh/blogserver/dto/request/BoardSearchCondition.java
@@ -1,5 +1,8 @@
 package com.yh.blogserver.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "게시글 검색조건으로 사용자가 프론트에서 요청하는 셀렉트 박스 값.")
 public enum BoardSearchCondition {
     TITLE,
     CONTENT,
