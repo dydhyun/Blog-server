9714e40ed871484281c2a475b35acc52fad5feb7
dydhyun <dydgus625@naver.com>
2026-01-21 16:34:24 +0900

    feat: userId로 User테이블을 조회해 BlogHeaderDto로 반환하는 쿼리 추가

--- src/main/java/com/yh/blogserver/repository/user/UserRepository.java
+++ src/main/java/com/yh/blogserver/repository/user/UserRepository.java
@@ -1,10 +1,15 @@
 package com.yh.blogserver.repository.user;
 
+import com.yh.blogserver.dto.response.BlogHeaderDto;
 import com.yh.blogserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,4 +19,22 @@ public interface UserRepository extends JpaRepository<User, Long> {
     long countByNickname(String userNickname);
 
     Optional<User> findByUserId(String userId);
+
+    @Query("""
+        select new com.yh.blogserver.dto.response.BlogHeaderDto(
+            u.userId,
+            u.nickname,
+            u.profileImageUrl,
+            u.description,
+            (
+                select count(b)
+                from Board b
+                where b.user = u
+                  and b.boardDeleteFlag = false
+            )
+        )
+        from User u
+        where u.userId in :userIds
+    """)
+    List<BlogHeaderDto> findBlogHeadersByUserIds(@Param("userIds") Set<String> userIds);
 }
