0d266f03d1f77052b98ed95c5870f81ae42fdb65
dydhyun <dydgus625@naver.com>
2026-01-21 16:24:22 +0900

    feat: BlogController 메인에서 사용 할 최신블로그 카드 조회API 추가

--- src/main/java/com/yh/blogserver/controller/blog/BlogController.java
+++ src/main/java/com/yh/blogserver/controller/blog/BlogController.java
@@ -1,10 +1,16 @@
 package com.yh.blogserver.controller.blog;
 
-import com.yh.blogserver.dto.response.ResponseDto;
-import com.yh.blogserver.dto.response.UserResponseDto;
-import com.yh.blogserver.service.user.UserService;
+import com.yh.blogserver.dto.response.*;
+import com.yh.blogserver.service.blog.BlogService;
 import com.yh.blogserver.util.message.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,26 +18,91 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Bolg API", description = "블로그, 게시글 조회 관련 API")
+import java.util.ArrayList;
+import java.util.List;
+
+@Tag(name = "Bolg API", description = "블로그, 사용자 + 게시글 조회 관련 API")
 @RestController
 @RequestMapping("/blogs")
 public class BlogController {
 
-    private final UserService userService;
+    private static final Logger log = LoggerFactory.getLogger(BlogController.class);
+    private final BlogService blogService;
 
-    public BlogController(UserService userService) {
-        this.userService = userService;
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
     }
 
-
     // Jmeter
-    @GetMapping("/{userId}")
-    public ResponseEntity<ResponseDto<UserResponseDto>> getUser(@PathVariable String userId){
+    @Operation(summary = "블로그 조회 API",
+            description = "메인에서 사용 될 블로그(유저 + 최신 게시글) 조회 API"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패")
+    })
+    @GetMapping()
+    public ResponseEntity<ResponseDto<List<MainBlogCardDto>>> getBlogs(){
+//        log.info("[MAIN 최신 3개의 글 블로그 조회 요청]");
 
-        UserResponseDto userResponseDto = userService.getUserByUserId(userId);
+        List<MainBlogCardDto> newestBlogs = blogService.getNewestBlogs();
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ResponseDto.success(userResponseDto, ResponseMessage.OK.message(), HttpStatus.OK.value()));
+                .body(ResponseDto.success(newestBlogs, ResponseMessage.OK.message(),HttpStatus.OK.value()));
+    }
+
+    @Operation(summary = "특정 유저 블로그 조회",
+            description = """
+                    유저 아이디를 PathVariable로 받아 특정유저 블로그를 조회합니다.
+                    블로그 주인 정보, 게시글 목록(페이징), 게시글 수 반환.
+                    *블로그 주인 정보와 게시글 목록은 데이터 변경 주기가 다릅니다.*
+                    """
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 블로그")
+    })
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseDto<BlogResponseDto>> getUserBlog(@PathVariable String userId, Pageable pageable) {
+        log.info("[GET USERS BLOG 요청] blogUserId={}", userId);
+
+
+        return null;
+    }
+
+    // SPA 최적화
+    @Operation(summary = "고정된 헤더로 사용 될 특정 유저 정보 조회.",
+            description = "유저 아이디를 PathVariable로 받아 특정 블로그의 유저 정보를 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 블로그")
+    })
+    @GetMapping("/{userId}/header")
+    public ResponseEntity<ResponseDto<BlogHeaderDto>> getBlogHeader(@PathVariable String userId) {
+        log.info("[GET BLOG HEADER 요청] blogUserId={}", userId);
+
+
+        return null;
+    }
+
+    @Operation(summary = "재요청이 많은 바디로 들어갈 유저의 글 목록 조회.",
+            description = "유저 아이디를 PathVariable로 받아 특정 블로그의 게시글 목록을 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 블로그")
+    })
+    @GetMapping("/{userId}/boards")
+    public ResponseEntity<ResponseDto<Page<BlogBoardSummaryDto>>> getBlogBoards(@PathVariable String userId,
+                                                                                Pageable pageable) {
+        log.info("[GET BLOG BOARDS 요청] blogUserId={}", userId);
+
+
+        return null;
     }
 
 
