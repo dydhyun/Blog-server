a6ffc8453506d4540446de466882dfcb6297ce27
dydhyun <dydgus625@naver.com>
2026-01-21 16:35:21 +0900

    feat: pageable을 이용해 최신게시글 조회 후 BlogBoardSummaryDto로 반환하는 쿼리 추가

--- src/main/java/com/yh/blogserver/repository/board/BoardRepository.java
+++ src/main/java/com/yh/blogserver/repository/board/BoardRepository.java
@@ -1,7 +1,9 @@
 package com.yh.blogserver.repository.board;
 
+import com.yh.blogserver.dto.response.BlogBoardSummaryDto;
 import com.yh.blogserver.entity.Board;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -27,4 +30,21 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<Board> findByBoardContentsContainingAndBoardDeleteFlagFalse(String keyword, Pageable pageable);
 
     Page<Board> findByUser_NicknameAndBoardDeleteFlagFalse(String keyword, Pageable pageable);
-}
+
+
+    @Query("""
+        select new com.yh.blogserver.dto.response.BlogBoardSummaryDto(
+            b.boardIndex,
+            b.user.userId,
+            b.boardTitle,
+            b.thumbnailUrl,
+            b.boardCreatedTime,
+            b.boardViewCnt
+        )
+        from Board b
+        where b.boardDeleteFlag = false
+        order by b.boardCreatedTime desc
+    """)
+    List<BlogBoardSummaryDto> findNewestBoards(Pageable pageable);
+
+    }
