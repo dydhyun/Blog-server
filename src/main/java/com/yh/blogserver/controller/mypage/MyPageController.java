3fdc9188ecdeafd438ffc539181c5936599b7124
dydhyun <dydgus625@naver.com>
2026-01-22 16:33:10 +0900

    feat: MyPageController 내 정보 조회 API 추가

--- src/main/java/com/yh/blogserver/controller/mypage/MyPageController.java
+++ src/main/java/com/yh/blogserver/controller/mypage/MyPageController.java
@@ -1,54 +1,63 @@
 package com.yh.blogserver.controller.mypage;
 
-import com.yh.blogserver.config.JwtTokenProvider;
 import com.yh.blogserver.dto.response.ResponseDto;
 import com.yh.blogserver.dto.response.UserResponseDto;
+import com.yh.blogserver.security.auth.CustomUserDetails;
+import com.yh.blogserver.service.mypage.MyPageUseCase;
+import com.yh.blogserver.util.message.ResponseMessage;
+import com.yh.blogserver.util.message.UserMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "MyPage API", description = "마이페이지 사용자 계정 관련 조회,수정,탈퇴 API")
 @RestController
-@RequestMapping("/myPage")
+@RequestMapping("/users/me")
 public class MyPageController {
+
+    private final MyPageUseCase myPageUseCase;
+
+    public MyPageController(MyPageUseCase myPageUseCase) {
+        this.myPageUseCase = myPageUseCase;
+    }
 
     @Operation(
             summary = "마이페이지 조회",
             description = """
-                    로그인된 사용자가 마이페이지를 조회합니다.
-                    Authorization 헤더에 JWT 토큰을 전달 해야합니다.
+                    로그인된 사용자가 개인정보를 수정할 수 있는 마이페이지를 조회합니다.
                     """
     )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "조회 성공"),
-            @ApiResponse(responseCode = "400", description = "조회 실패 (토큰 만료 등)")
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "조회 실패")
     })
     @GetMapping("")
-    public ResponseEntity<ResponseDto<UserResponseDto>> getMyPage(){
+    public ResponseEntity<ResponseDto<UserResponseDto>> getMyPage(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        String userId = customUserDetails.getUserId();
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ResponseDto.success(null,"", HttpStatus.OK.value()));
+        UserResponseDto responseDto = myPageUseCase.getMyPage(userId);
+
+        return ResponseEntity.ok(ResponseDto.success(
+                responseDto, ResponseMessage.OK.message(), HttpStatus.OK.value()));
     }
 
     @PatchMapping("")
-    public ResponseEntity<ResponseDto<UserResponseDto>> patchMyPage(){
+    public ResponseEntity<ResponseDto<Void>> patchMyPage(){
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ResponseDto.success(null,"", HttpStatus.OK.value()));
+        return ResponseEntity.ok(ResponseDto.success(
+                null, UserMessage.USER_INFO_CHANGE.message(), UserMessage.USER_INFO_CHANGE.code()));
     }
 
-    @PatchMapping("/delete")
-    public ResponseEntity<ResponseDto<UserResponseDto>> deleteMyAccount(){
+    @DeleteMapping("")
+    public ResponseEntity<ResponseDto<Void>> deleteMyAccount(){
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ResponseDto.success(null,"", HttpStatus.OK.value()));
+        return ResponseEntity.ok(ResponseDto.success(
+                null, UserMessage.USER_DELETE_SUCCESS.message(), UserMessage.USER_DELETE_SUCCESS.code()));
     }
 
 
