0657bf9a953484552a5611d39eda09b82937e9c3
dydhyun <dydgus625@naver.com>
2026-01-22 10:33:11 +0900

    feat: BoardRepository board테이블 조회 후 BlogBoardSummaryDto형태로 반환하는 쿼리 추가

--- src/main/java/com/yh/blogserver/repository/board/BoardRepository.java
+++ src/main/java/com/yh/blogserver/repository/board/BoardRepository.java
@@ -3,11 +3,11 @@ package com.yh.blogserver.repository.board;
 import com.yh.blogserver.dto.response.BlogBoardSummaryDto;
 import com.yh.blogserver.entity.Board;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -47,4 +47,19 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     """)
     List<BlogBoardSummaryDto> findNewestBoards(Pageable pageable);
 
-    }
+    @Query("""
+        select new com.yh.blogserver.dto.response.BlogBoardSummaryDto(
+            b.boardIndex,
+            b.user.userId,
+            b.boardTitle,
+            b.thumbnailUrl,
+            b.boardCreatedTime,
+            b.boardViewCnt
+        )
+        from Board b
+        where b.user.userId = :userId
+        and b.boardDeleteFlag = false
+        order by b.boardCreatedTime desc
+    """)
+    Page<BlogBoardSummaryDto> findBoardSummariesByUserId(@Param("userId") String userId, Pageable pageable);
+}
