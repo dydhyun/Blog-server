d6a837a074d199fad9eccc1fad0195bbc3c9795b
dydhyun <dydgus625@naver.com>
2026-01-29 13:54:14 +0900

    feat: AdminUseCase 삭제유저 조회 메서드 추가

---
+++ src/main/java/com/yh/blogserver/service/admin/AdminUseCase.java
@@ -0,0 +1,11 @@
+package com.yh.blogserver.service.admin;
+
+import com.yh.blogserver.dto.response.PageResponse;
+import com.yh.blogserver.dto.response.UserResponseDto;
+import org.springframework.data.domain.Pageable;
+
+public interface AdminUseCase {
+
+    PageResponse<UserResponseDto> getDeletedUsers(Pageable pageable);
+
+}
---
+++ src/main/java/com/yh/blogserver/service/admin/AdminUseCaseImpl.java
@@ -0,0 +1,28 @@
+package com.yh.blogserver.service.admin;
+
+import com.yh.blogserver.dto.response.PageResponse;
+import com.yh.blogserver.dto.response.UserResponseDto;
+import com.yh.blogserver.service.board.BoardService;
+import com.yh.blogserver.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AdminUseCaseImpl implements AdminUseCase{
+
+    private final UserService userService;
+    private final BoardService boardService;
+
+    public AdminUseCaseImpl(UserService userService, BoardService boardService) {
+        this.userService = userService;
+        this.boardService = boardService;
+    }
+
+    // 정렬기능 추가
+    @Override
+    public PageResponse<UserResponseDto> getDeletedUsers(Pageable pageable) {
+        return userService.getDeletedUser(pageable);
+    }
+}
