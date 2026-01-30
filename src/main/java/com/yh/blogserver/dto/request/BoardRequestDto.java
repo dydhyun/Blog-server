e87965a0079d40fb72e6865e618e7125ee0fab4a
dydhyun <dydgus625@naver.com>
2026-01-30 15:25:32 +0900

    refactor: BoardRequestDto User필드 삭제

--- src/main/java/com/yh/blogserver/dto/request/BoardRequestDto.java
+++ src/main/java/com/yh/blogserver/dto/request/BoardRequestDto.java
@@ -2,6 +2,7 @@ package com.yh.blogserver.dto.request;
 
 import com.yh.blogserver.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -11,10 +12,12 @@ import java.time.LocalDateTime;
 public record BoardRequestDto(
         @Schema(description = "게시글 인덱스. 사용자 입력 x", example = "1")
         Long boardIndex,
-        @Schema(description = "게시글 작성자. 사용자 입력 x", example = "토큰으로 유저아이디 추출해서 유저 정보 세팅중.")
-        User user,
+//        @Schema(description = "게시글 작성자. 사용자 입력 x", example = "토큰으로 유저아이디 추출해서 유저 정보 세팅중.")
+//        User user,
+        @NotBlank
         @Schema(description = "게시글 제목. 빈 제목 불가", example = "게시글 1")
         String boardTitle,
+        @NotBlank
         @Schema(description = "게시글 내용. 빈 내용 불가", example = "게시글 1 내용 입니다.")
         String boardContents,
         @Schema(description = "게시글 생성시간. 사용자 입력 x", example = "2025-11-20 17:42:26.517182")
